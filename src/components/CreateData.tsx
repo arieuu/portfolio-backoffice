ba5fa9773dab72c43b350c736480565bfb18c6d6
Pliskin <arieljanickcarvalho@gmail.com>
2023-10-16 20:56:14 -0100

    Worked create data form.
    Made the form, validating the data with schema and getting the data from the fields.

--- src/components/CreateData.tsx
+++ src/components/CreateData.tsx
@@ -1,10 +1,59 @@
-import { Heading } from "@chakra-ui/react";
+import { Alert, AlertIcon, AlertTitle, Button, Flex, FormControl, FormLabel, Heading, Input, Textarea } from "@chakra-ui/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { string, z } from "zod";
 
+const schema = z.object({
+    type: z.string().min(4, {message: "Type too short"}),
+    title: z.string().min(4, {message: "Title too short"}),
+    content: z.string().min(8, {message: "Content too short"}),
+    extra: z.string().min(8, {message: "Extra content too short"}),
+});
+
+// Getting object shape from our created zod schema object
+type SchemaShape = z.infer<typeof schema>;
 
 const CreateData = () => {
 
+    const { register, handleSubmit, formState: { errors }} = useForm<SchemaShape>({ resolver: zodResolver(schema)});
+
+    const onSubmit: SubmitHandler<SchemaShape> = (data) => {
+
+        console.log(data.title);
+        console.log(data.content);
+    }
+
     return(
-        <Heading> Create Data </Heading>
+        <Flex flexDirection="column" alignItems="center" justifyContent="center" p={8} px={32}>
+            <Heading mb={"24"}> Create data </Heading>
+
+            <FormControl as="form" onSubmit={handleSubmit(onSubmit)}>
+
+                <FormLabel>Data type</FormLabel>
+                <Input id="type" {...register("type")}  type='text' placeholder="Insert data type" border="1px black solid" mb={7} autoFocus/>
+
+                { (errors.type) && <Alert mb={7} status='error'> <AlertIcon /> <AlertTitle> {errors.type?.message}</AlertTitle> </Alert> }
+
+
+                <FormLabel>Title</FormLabel>
+                <Input id="title" {...register("title")} type='text' placeholder="Insert data title" border="1px black solid" mb={7}/>
+
+                { (errors.title) && <Alert mb={7} status='error'> <AlertIcon /> <AlertTitle> {errors.title?.message}</AlertTitle> </Alert> }
+
+                <FormLabel>Content</FormLabel>
+                <Textarea id="content" {...register("content")} placeholder="Insert data content" border="1px black solid" mb={7}/>
+
+                { (errors.content) && <Alert mb={7} status='error'> <AlertIcon /> <AlertTitle> {errors.content?.message}</AlertTitle> </Alert> }
+
+                <FormLabel>Extra content</FormLabel>
+                <Textarea id="extra" {...register("extra")} placeholder="Insert extra data content" border="1px black solid" mb={7}/>
+                
+                { (errors.extra) && <Alert mb={7} status='error'> <AlertIcon /> <AlertTitle> {errors.extra?.message}</AlertTitle> </Alert> }
+
+                <Button type="submit" colorScheme="linkedin"> Create data </Button>
+            </FormControl>
+            
+        </Flex>
     )
 
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,8 @@
-import { Alert, AlertIcon, AlertTitle, Button, Flex, Heading, Input, Stack, Text } from "@chakra-ui/react";
+import { Alert, AlertIcon, AlertTitle, Button, Flex, Heading, Input, Stack } from "@chakra-ui/react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 import useAuth from "../hooks/useAuth";
-import { Link } from "react-router-dom";
 
 const schema = z.object({
     username: z.string().min(4, {message: "Username too short"}),
@@ -20,7 +19,8 @@ const Login = () => {
 
     const onSubmit: SubmitHandler<SchemaShape> = (data) => {
 
-        /* We don't even need to prevent form default behaviour.
+        /* 
+           We don't even need to prevent form default behaviour.
            We Also don't need to check if the data has been received because
            the form won't submit until everything has been validated
         */
@@ -43,19 +43,19 @@ const Login = () => {
 
                         <Input id="username" {...register("username")} autoFocus variant="filled" placeholder="Username" size="lg" width="400px" height="60px" focusBorderColor="black"/>
 
-                            { (errors.username ) && <Alert status='error'>
-                                <AlertIcon />
-                                <AlertTitle> {errors.username?.message}</AlertTitle>
-                                </Alert>
-                            }
+                        { (errors.username ) && <Alert status='error'>
+                            <AlertIcon />
+                            <AlertTitle> {errors.username?.message}</AlertTitle>
+                            </Alert>
+                        }
 
                         <Input id="password" {...register("password")} variant="outline" type="password" placeholder="Password" size="lg" width="400px" height="60px" focusBorderColor="black"/>
 
-                            { (errors.password) && <Alert status='error'>
-                                <AlertIcon />
-                                <AlertTitle> {errors.password?.message}</AlertTitle>
-                                </Alert>
-                            }
+                        { (errors.password) && <Alert status='error'>
+                            <AlertIcon />
+                            <AlertTitle> {errors.password?.message}</AlertTitle>
+                            </Alert>
+                        }
 
                         <Button size="lg" type="submit" isDisabled={isLoading}> Login </Button>
 
