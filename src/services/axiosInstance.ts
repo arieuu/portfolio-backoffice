64f233daf553a1d091d1aecafaad49272b468b0b
Pliskin <arieljanickcarvalho@gmail.com>
2023-11-03 16:30:36 -0100

    Changed base url

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: "http://api.arielcarvalho.io:3000/api/v1",
+    baseURL: "http://backend.arielcarvalho.io:3000/api/v1",
 });
 
 export default axiosInstance;
