b35603b6f5b97afac5515a42e8207f6eefbc4666
Pliskin <arieljanickcarvalho@gmail.com>
2023-11-04 20:29:41 -0100

    Added new secure (https) api base url

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: "http://backend.arielcarvalho.io:3000/api/v1",
+    baseURL: "https://api.arielcarvalho.io/api/v1",
 });
 
 export default axiosInstance;
